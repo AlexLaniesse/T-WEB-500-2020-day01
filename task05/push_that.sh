c0df0e1bcfb84f3cb48826675c160c1c74b0762e
alaniesse <alaniesse@localhost.localdomain>
2020-09-14 11:09:56 -0400

    gg

--- task05/push_that.sh
+++ task05/push_that.sh
@@ -1,4 +1,14 @@
 #!/bin/bash  
 git add * 
+git commit -m "$(read -p 'Commit description: ')"
 git commit -m "$1"
 git push
+
+//./mr_clean
+//git commit -m "$1"
+//git add . && \
+//git commit -m "$(read -p 'Commit description: ')" && \
+//git pull && \
+//git Push Origin master && \
+//git status
+
